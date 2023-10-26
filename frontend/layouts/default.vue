c90015735aa9e9a2bb46ab737d0175bf13d7fa43
TruyenBD <mrtruyenbd1407@gmail.com>
2023-10-27 03:10:42 +0700

    fix: remove .nuxt

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="default">
+        <h2>xinchoa</h2>
+        <nuxt class="body"/>
+    </div>
+  </template>
+  
+  <script>
+  import { mapActions, mapGetters } from 'vuex'
+//   import TabLeft from '~/components/TabLeft.vue'
+//   import TopNavBar from '~/components/TopNavBar.vue'
+  
+  export default {
+    components: {
+      TabLeft,
+      TopNavBar,
+    },  
+    data() {
+      return {
+        statusTabLeft: 'openTabLeft',
+      }
+    },
+    methods: {
+    },
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  .default {
+    display: flex;
+    flex-direction: row;
+    background-color: $background-color;
+    height: 100vh;
+    overflow: hidden;
+    &__body {
+      display: flex;
+      flex-direction: column; 
+      width: calc(100% - 239px);
+      height: 100%;
+      // gap: 20px;
+      &.closeTabLeft {
+        width: calc(100% - 55px);
+      }
+      &__tableft {
+        height: 100%;
+        max-width: 239px;
+      }
+  
+      .body {
+        // flex-basis: calc(100% - 250px);
+  
+        &.closeTabLeft {
+          flex-basis: auto;
+        }
+      }
+    }
+  }
+  </style>
+  
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <h2>chao</h2>
 </template>
 
 <script>
